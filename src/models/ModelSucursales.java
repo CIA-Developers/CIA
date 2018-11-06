5af64ba06ee5444b69dd0d182a9fffce8ca63bce
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-05 22:58:06 -0600

    se imortan las librerias necesarias para la funcion de la tabla en la vista Sucursales

--- src/models/ModelSucursales.java
+++ src/models/ModelSucursales.java
@@ -4,6 +4,17 @@
  * and open the template in the editor.
  */
 package models;
+import conectar_tablas.Database; //llamamos la conexion a la BD para almacen
+import static conectar_tablas.Database.getConexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
