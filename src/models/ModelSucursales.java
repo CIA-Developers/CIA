78698999a9ab089a0a74c7bf0558eafd2a1af098
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-05 23:25:32 -0600

    conexion y consulta requerida para la tabla de ViewSucursales

--- src/models/ModelSucursales.java
+++ src/models/ModelSucursales.java
@@ -21,5 +21,30 @@ import javax.swing.table.DefaultTableModel;
  * @author Octaviano
  */
 public class ModelSucursales {
+    DefaultTableModel model;
+    private Connection conexion;     
+    private Statement st;     
+    private ResultSet rs;
+    PreparedStatement ps;
+    
+    /**
+     * se hace la conexion a la Base de datos y se hace la consulta hacia la tabla de sucursales 
+     * que tiene una union con la tabla de sucursales_productos 
+     * y finalmente con la tabla de produtos para obtener el nombre del producto
+     * que se tiene en cada sucursal 
+     */
+    public void Conectar(){
+             try{ 
+                conexion=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/stockcia","root","");                     
+                st=conexion.createStatement(); 
+                rs=st.executeQuery("SELECT sucursal.no_sucursal,productos.nom_producto, calle, colonia, numero, telefono,sucursal_productos.codigo_producto, existencias, limite_maximo, limite_minimo from sucursal inner join sucursal_productos on sucursal.no_sucursal = sucursal_productos.no_sucursal inner join productos on productos.codigo_producto = sucursal_productos.codigo_producto;");
+                        
+                rs.first();
+                
+            }catch(SQLException err){ 
+                JOptionPane.showMessageDialog(null,"Error "+err.getMessage()); 
+            } 
+
+    } 
        
 }
