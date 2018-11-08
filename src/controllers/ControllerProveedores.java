f5629c7804c9c6e6c73b667df92854386d43657a
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-08 08:19:22 -0600

    Se agrega a la tabla el evento de MouseListener

--- src/controllers/ControllerProveedores.java
+++ src/controllers/ControllerProveedores.java
@@ -4,60 +4,64 @@
  * and open the template in the editor.
  */
 package controllers;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import models.ModelProveedores;
 import views.ViewProveedores;
+
 /**
  *
  * @author Octaviano
  */
 public class ControllerProveedores {
+
     public ModelProveedores modelProveedores;
-    public ViewProveedores viewProveedores; 
-     MouseListener ml = new MouseListener(){
+    public ViewProveedores viewProveedores;
+    MouseListener ml = new MouseListener() {
         @Override
         public void mouseClicked(MouseEvent e) {
-          if (e.getSource() == viewProveedores.jt_vista) {
+            if (e.getSource() == viewProveedores.jt_vista) {
                 jt_vista_MouseClicked();
-            }  
+            }
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-           
+
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-           
+
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            
+
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-           
+
         }
-        
+
     };
-    
-    ActionListener actionListener = new ActionListener(){
+
+    ActionListener actionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
 
-        }     
+        }
     };
 
     public ControllerProveedores(ModelProveedores modelProveedores, ViewProveedores viewProveedores) {
         this.modelProveedores = modelProveedores;
         this.viewProveedores = viewProveedores;
+        this.viewProveedores.jt_vista.addMouseListener(ml);//agregar a la table el evento de MouseListener
+        ConexionBD();
     }
-      
+
 }
