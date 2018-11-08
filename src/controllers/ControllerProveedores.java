2cb61db3600af4dc979d0a70d3fc13cb39c5249e
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-08 08:17:37 -0600

    Agregar metodo a ControllerProveedores

--- src/controllers/ControllerProveedores.java
+++ src/controllers/ControllerProveedores.java
@@ -8,9 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import models.ModelSucursales;
-import views.ViewSucursales;
-
 import models.ModelProveedores;
 import views.ViewProveedores;
 /**
@@ -20,10 +17,40 @@ import views.ViewProveedores;
 public class ControllerProveedores {
     public ModelProveedores modelProveedores;
     public ViewProveedores viewProveedores; 
+     MouseListener ml = new MouseListener(){
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          if (e.getSource() == viewProveedores.jt_vista) {
+                jt_vista_MouseClicked();
+            }  
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+           
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+           
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+           
+        }
+        
+    };
     
     ActionListener actionListener = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {
+            
 
         }     
     };
