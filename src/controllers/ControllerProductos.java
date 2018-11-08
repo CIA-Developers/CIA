a79bf513ffa42e7de2cfd882555ff2f970240f40
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-07 20:43:04 -0600

    Controlador listo, para ViewProductos

--- src/controllers/ControllerProductos.java
+++ src/controllers/ControllerProductos.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import models.ModelProductos;
 import views.ViewProductos;
+
 /**
  *
  * @author Octaviano
@@ -26,7 +27,18 @@ public class ControllerProductos {
     public ControllerProductos(ModelProductos modelProductos, ViewProductos viewProductos) {
         this.modelProductos = modelProductos;
         this.viewProductos = viewProductos;
+        ConexionBD(); //se llama a este metodo para obtener los datos en la tabla
     }
     
+    /**
+     * este metodo hace la conexion a la base de datos 
+     * llama a los metodos conectar, mostrar  dentro del modelo 
+     * muestra en la tabla los datos que contiene la variable de modelo_productos
+     */
+    public void ConexionBD(){
+        modelProductos.Conectar();
+        modelProductos.mostrar();
+        viewProductos.jt_vista.setModel(modelProductos.getModelo_productos()); //asignar a la tabla los valores correspondientes
+    }
     
 }
