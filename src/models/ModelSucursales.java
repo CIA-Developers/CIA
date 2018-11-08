4b2df3a0c24abf5ec72f884fa9ef41dfb00e8c47
angelesOct <1717110255@utectulancingo.edu.mx>
2018-11-08 08:41:50 -0600

    Declaracion de nuevas variables en modelSucursales

--- src/models/ModelSucursales.java
+++ src/models/ModelSucursales.java
@@ -41,17 +41,148 @@ public class ModelSucursales {
     public void setRec(int rec) {
         this.rec = rec;
     }
+    /**
+     * Variables para el metodo de busqueda
+     */
+    public String[] titulos = {"No sucursal", "Calle", "Colonia", "Numero","Telefono", "Codigo_producto", "Nombre Producto", "Stock", "Stok maximo", "Stock minimo"}; //columnas de la tabla
+
+    public String[] getTitulos() {
+        return titulos;
+    }
+
+    public void setTitulos(String[] titulos) {
+        this.titulos = titulos;
+    }
     
-  
-    DefaultTableModel model;
+    public String[] registros = new String[50];
+
+    public String[] getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(String[] registros) {
+        this.registros = registros;
+    }
+    
+    public String sql;
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+    //Variables que corresponden a cada caja de texto
+    public String sucursal;
+    public String calle;
+    public String colonia;
+    public String numero;
+    public String Telefono;
+    public String codigo_producto;
+    public String nombre_producto;
+    public String stock;
+    public String stock_maximo;
+    public String Strock_minimo;
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCodigo_producto() {
+        return codigo_producto;
+    }
+
+    public void setCodigo_producto(String codigo_producto) {
+        this.codigo_producto = codigo_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getStock_maximo() {
+        return stock_maximo;
+    }
+
+    public void setStock_maximo(String stock_maximo) {
+        this.stock_maximo = stock_maximo;
+    }
+
+    public String getStrock_minimo() {
+        return Strock_minimo;
+    }
+
+    public void setStrock_minimo(String Strock_minimo) {
+        this.Strock_minimo = Strock_minimo;
+    }
+    
+    
+      
+    DefaultTableModel model; // variable que usa para el metodo de buscar
+
+    public DefaultTableModel getModel() {
+        return model;
+    }
+
+    public void setModel(DefaultTableModel model) {
+        this.model = model;
+    }
+    
     private Connection conexion;     
     private Statement st;     
     private ResultSet rs;
     PreparedStatement ps;
     
-    
-    
-    
+       
     /**
      * se hace la conexion a la Base de datos y se hace la consulta hacia la tabla de sucursales 
      * que tiene una union con la tabla de sucursales_productos 
@@ -92,7 +223,6 @@ public class ModelSucursales {
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
-       
+         
 }
