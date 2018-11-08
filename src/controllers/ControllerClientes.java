eb05c93225399dac9fd74f13706918076e25fcce
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-08 08:40:03 -0600

    Creacion de eventos en ControllerClientes

--- src/controllers/ControllerClientes.java
+++ src/controllers/ControllerClientes.java
@@ -6,6 +6,8 @@
 package controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import models.ModelClientes;
 import views.ViewClientes;
 
@@ -16,6 +18,37 @@ import views.ViewClientes;
 public class ControllerClientes {
    public ModelClientes modelClientes; 
    public ViewClientes viewClientes; 
+    MouseListener ml = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (e.getSource() == viewClientes.jt_vista) {
+                jt_vista_MouseClicked();
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+    };
+   
+   
    
    ActionListener actionListener = new ActionListener(){
        @Override
