dc45e69a089e788687d806822c1adeee8827dc54
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-07 20:06:09 -0600

    Cambios en tipo de Dato en la BD

--- src/bd/bd_punto_venta.sql
+++ src/bd/bd_punto_venta.sql
@@ -15,17 +15,18 @@ CREATE TABLE productos (
     precio_unitario_venta FLOAT NOT NULL,
     Unidad VARCHAR(15) NOT NULL,
     descripcion VARCHAR(25) NOT NULL,
-    fecha_ingreso DATE NOT NULL,
+    fecha_ingreso TIMESTAMP NOT NULL,
     existencia_total INT(25) NOT NULL,
-    status_prod VARCHAR(15) NOT NULL,
+    status_prod VARCHAR ( 15 ) NOT NULL,
     image BLOB);
     
     insert into productos (codigo_producto,nom_producto,tipo_producto,marca,precio_unitario_venta,unidad,descripcion,fecha_ingreso,existencia_total,status_prod) values
-        (1651,"lamina galvanizada calibre 16 de 4 x 8","construccion","Vimar",105.00,"pza","Lamina de acero con recubrimiento de zinc",2018-05-01,727,"promocion"),
-        (1722,"Tuerca hexagonal de 5/8","ferreteria","Fastenere",0.00,"kg","Tuerca con una insercion de collar de nylon que resiste el giro.",2018-03-02,1308,"normal"),
-        (1658,"Tubo rectangular aluminio","construccion","Bosch",389.00,"pza","Tubo de 1m de lago y 12 mm de ancho ",2018-06-23,1098,"normal"),
-        (1321,"Clavos","ferreteria","Sithl",50.00,"kg","Clavos Estandar 3-2-1",2018-07-20,3330,"normal"),
-        (8170,"Motosierra" ,"jardin","Truper",500.00,"pza","Motosierra Electrica 18 pulgadas",2018-04-15,27,"promocion");
+        (1651,"lamina galvanizada calibre 16 de 4 x 8","construccion","Vimar",105.00,"pza","Lamina de acero con recubrimiento de zinc","2018-05-01",727,"en venta"),
+        (1722,"Tuerca hexagonal de 5/8","ferreteria","Fastenere",0.00,"kg","Tuerca con una insercion de collar de nylon que resiste el giro.","2018-03-02",1308,"en venta"),
+        (1658,"Tubo rectangular aluminio","construccion","Bosch",389.00,"pza","Tubo de 1m de lago y 12 mm de ancho ","2018-06-23",1098,"en venta"),
+        (1321,"Clavos","ferreteria","Sithl",50.00,"kg","Clavos Estandar 3-2-1","2018-07-20",3330,"en venta"),
+        (8170,"Motosierra" ,"jardin","Truper",500.00,"pza","Motosierra Electrica 18 pulgadas","2018-04-15",27,"en venta");
+    
     
 CREATE TABLE sucursal (
     no_sucursal INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -93,12 +94,14 @@ CREATE TABLE promocion_prod (
     id_promocion_d INT(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     codigo_producto VARCHAR(10) NOT NULL,
     id_promociones INT(15) NOT NULL,
+    fecha_inicio TIMESTAMP NOT NULL,
+    fecha_final TIMESTAMP NOT NULL,
     FOREIGN KEY (codigo_producto) REFERENCES productos (codigo_producto),
     FOREIGN KEY (id_promociones) REFERENCES promociones (id_promociones));
     
     insert into promocion_prod values 
-        (1,1651,1),
-        (2,8170,2);
+        (1,1651,1,"2018-08-16","2018-10-16"),
+        (2,8170,2,"2018-08-16","2018-10-16");
 
 CREATE TABLE empleados_compras (
     RFC_empl_comp CHAR(13) NOT NULL PRIMARY KEY,
@@ -114,11 +117,11 @@ CREATE TABLE empleados_compras (
     fecha_ingreso_comp timestamp NOT NULL);
     
     insert into empleados_compras values 
-        ("BAFJ701213SB1","Juan","Barrios","Fernández","hombre","casado",5557542100,"juan@email.com","barrios69",1234,2018-01-01),
-        ("MAHM670102N01","Manuel","Martinez","Hernández","hombre","divorciado",5547896332,"manu@outlook.com","manu15",1234,2018-05-03),
-        ("RASL75112LM50","Maria Luisa","Ramirez","Sanchez","mujer","soltero",7755896412,"luisa12@hotmail.com","mariaR",1234,2018-02-02),
-        ("ADD7808121G80","Dolores","San Martín","Davilos","mujer","soltero",7751200145,"dolores@hotmail.com","doloresMart",1234,2018-03-15),
-        ("CALF750228LK7","Felipe","Camargo","Lozano","hombre","soltero",7756632114,"felipelozano@gmail.com","FelipeC",1234,2018-08-12);
+        ("BAFJ701213SB1","Juan","Barrios","Fernández","hombre","casado",5557542100,"juan@email.com","barrios69",1234,"2018-01-01"),
+        ("MAHM670102N01","Manuel","Martinez","Hernández","hombre","divorciado",5547896332,"manu@outlook.com","manu15",1234,"2018-05-03"),
+        ("RASL75112LM50","Maria Luisa","Ramirez","Sanchez","mujer","soltero",7755896412,"luisa12@hotmail.com","mariaR",1234,"2018-02-02"),
+        ("ADD7808121G80","Dolores","San Martín","Davilos","mujer","soltero",7751200145,"dolores@hotmail.com","doloresMart",1234,"2018-03-15"),
+        ("CALF750228LK7","Felipe","Camargo","Lozano","hombre","soltero",7756632114,"felipelozano@gmail.com","FelipeC",1234,"2018-08-12");
 
 
 CREATE TABLE proveedores (
@@ -153,11 +156,11 @@ CREATE TABLE compra (
     FOREIGN KEY ( RFC_empleado_comp) REFERENCES empleados_compras (RFC_empl_comp));
     
     insert into compra values 
-        (1,2,2018-05-12,5070,316.88,5386.88,"BAFJ701213SB1",1),
-        (2,1,2018-03-23,4475,279.69,4754.69,"MAHM670102N01",2),
-        (3,3,2018-11-01,1440,90.00,1530.00,"RASL75112LM50",3),
-        (4,5,2018-07-20,3800,237.50,4037.50,"ADD7808121G80",3),
-        (5,4,2018-10-20,10825,676.56,11501.56,"CALF750228LK7",4);
+        (1,2,"2018-05-12",5070,316.88,5386.88,"BAFJ701213SB1",1),
+        (2,1,"2018-03-23",4475,279.69,4754.69,"MAHM670102N01",2),
+        (3,3,"2018-11-01",1440,90.00,1530.00,"RASL75112LM50",3),
+        (4,5,"2018-07-20",3800,237.50,4037.50,"ADD7808121G80",3),
+        (5,4,"2018-10-20",10825,676.56,11501.56,"CALF750228LK7",4);
 
 CREATE TABLE detalle_compra(
     id_detalle_compra int(5) not null AUTO_INCREMENT primary key,
@@ -195,11 +198,11 @@ CREATE TABLE empleados_ventas (
     fecha_ingreso_vent timestamp NOT NULL);
 
     insert into empleados_ventas values 
-        ("CAGC800503021","Cinthia","Cazarez","Garcia","mujer","soltero",5575285866,"cintia@gmail.com","cinita_garcia",1234,2018-02-05),
-        ("TCHM830304102","Maria","Torres","Hernández","mujer","soltero",7538907451,"mari@email.com","Maria_torres",1234,2018-03-15),
-        ("CAJJ790305145","Javier","Carrasco","Jimenez","hombre","soltero",7757536987,"javier@outlook.com","Javier_carraso",1234,2018-05-04),
-        ("RIM8840305102","Benito","Rivera","Martinez","hombre","casado",7757589641,"benitoR@hotmail.com","Beniro_rivera",1234,2018-03-20),
-        ("PIPL850306120","Lilia","Prieto","Perez","mujer","casado",7757541230,"lilia@hootmail.com","Lilia_prieto",1234,2018-01-01);
+        ("CAGC800503021","Cinthia","Cazarez","Garcia","mujer","soltero",5575285866,"cintia@gmail.com","cinita_garcia",1234,"2018-02-05"),
+        ("TCHM830304102","Maria","Torres","Hernández","mujer","soltero",7538907451,"mari@email.com","Maria_torres",1234,"2018-03-15"),
+        ("CAJJ790305145","Javier","Carrasco","Jimenez","hombre","soltero",7757536987,"javier@outlook.com","Javier_carraso",1234,"2018-05-04"),
+        ("RIM8840305102","Benito","Rivera","Martinez","hombre","casado",7757589641,"benitoR@hotmail.com","Beniro_rivera",1234,"2018-03-20"),
+        ("PIPL850306120","Lilia","Prieto","Perez","mujer","casado",7757541230,"lilia@hootmail.com","Lilia_prieto",1234,"2018-01-01");
 
 CREATE TABLE clientes (
     RFC_cliente CHAR(13) NOT NULL PRIMARY KEY,
@@ -249,11 +252,11 @@ CREATE TABLE ventas(
     FOREIGN KEY(codigo_descuento ) REFERENCES descuentos (codigo_descuento));
     
     insert into ventas values 
-    (1,"CLIENTEGENERA",2018-02-10,152.47,8.96875,143.50,"null","CAGC800503021","efectivo",1,1,0),
-    (2,"HEOI231DFR456",2018-05-04,63.75,3.75,60.00,1,"TCHM830304102","efectivo",2,2,10),
-    (3,"GAOA123EDFR4",2018-05-15,451.56,26.5625,425.00,2,"CAJJ790305145","efectivo",4,2,10),
-    (4,"DIRA123DFR56T",2018-06-01,2298.19,135.1875,2163.00,3,"RIM8840305102","efectivo",5,3,10),
-    (5,"HERA123DFR456",2018-07-10,79.69,4.6875,75.00,4,"PIPL850306120","efectivo",3,4,10);
+    (1,"CLIENTEGENERA","2018-02-10",152.47,8.96875,143.50,"null","CAGC800503021","efectivo",1,1,0),
+    (2,"HEOI231DFR456","2018-05-04",63.75,3.75,60.00,1,"TCHM830304102","efectivo",2,2,10),
+    (3,"GAOA123EDFR4","2018-05-15",451.56,26.5625,425.00,2,"CAJJ790305145","efectivo",4,2,10),
+    (4,"DIRA123DFR56T","2018-06-01",2298.19,135.1875,2163.00,3,"RIM8840305102","efectivo",5,3,10),
+    (5,"HERA123DFR456","2018-07-10",79.69,4.6875,75.00,4,"PIPL850306120","efectivo",3,4,10);
 
 CREATE TABLE detalle_ventas (
     Id_detalle_venta INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
