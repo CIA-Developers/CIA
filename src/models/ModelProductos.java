0fa1f014e76e6a6bf622a40593faeddb2b11e729
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-08 21:51:04 -0600

    Creando variables para Boton Nuevo en viewProductos

--- src/models/ModelProductos.java
+++ src/models/ModelProductos.java
@@ -40,6 +40,28 @@ public class ModelProductos {
         this.rec = rec;
     } 
     
+    //*************************Variables que corresponden a los BOTONES*************************************
+    
+    public String Limpiar=" ";
+    public int codigo=0;
+
+    public String getLimpiar() {
+        return Limpiar;
+    }
+
+    public void setLimpiar(String Limpiar) {
+        this.Limpiar = Limpiar;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
     DefaultTableModel model;
     private Connection conexion;     
     private Statement st;     
