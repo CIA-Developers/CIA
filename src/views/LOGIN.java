9c070cf9abccd7795b319706a6b395d4932f30f3
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-11 22:03:42 -0600

    Haciendo transparente la vetana de login

--- src/views/LOGIN.java
+++ src/views/LOGIN.java
@@ -16,6 +16,8 @@ public class LOGIN extends javax.swing.JFrame {
      */
     public LOGIN() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setOpacity(0.5f);
     }
 
     /**
@@ -28,16 +30,17 @@ public class LOGIN extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 387, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 477, Short.MAX_VALUE)
         );
 
         pack();
