09c27049d8bfafbdc43037c15340e46ed2b0cbb1
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-12 20:52:31 -0600

    Nuevos metodos en controllerLogin

--- src/controllers/ControllerLogin.java
+++ src/controllers/ControllerLogin.java
@@ -27,10 +27,38 @@ public class ControllerLogin {
     private ControllerEmpleadosCompras controllerEmpleadosCompras;
     private ControllerEmpleadosVentas controllerEmpleados;
 
+    /**
+     * Controlador principal donde se un el modelo y controlador de cada menu prinipal
+     * Recibe cada controlador de las vistas de los menus
+     * dentro de este controlador se tiene el accesso a la programacion 
+     * en el controlador de cada menu
+     * @param modelLogin
+     * @param viewLogin
+     * @param controllers 
+     */
     public ControllerLogin(ModelLogin modelLogin, ViewLogin viewLogin, Object[] controllers) {
         this.modelLogin = modelLogin;
         this.viewLogin = viewLogin;
         this.controllers = controllers;
+        initComponets();
+        setControllers();
+    }
+    
+    /**
+     * Separa cada uno de los controlladores almacendados en controllers, de
+     * esta forma se puede acceder a todas las variables y métodos publicos
+     * de cada uno.
+     */
+    public void setControllers() {
+        controllerMenuAdmin = (ControllerMenuAdmin) controllers[0];
+    }
+    /**
+     * mustra la ventana principal del menuAdmin
+     */
+    private void initComponets(){
+        viewLogin.setTitle("LOGIN");
+        viewLogin.setLocationRelativeTo(null);
+        viewLogin.setVisible(true);
     }
 
 }
