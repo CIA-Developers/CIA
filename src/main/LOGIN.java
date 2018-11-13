08c51c210f2b0389b6b3b4049660ebf40cb8d763
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-12 20:53:14 -0600

    mainLogin (incompleto)

--- src/main/LOGIN.java
+++ src/main/LOGIN.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import models.*;
 import views.*;
+import controllers.*;
 /**
  *
  * @author Octaviano
@@ -18,7 +19,21 @@ public class LOGIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Object[] controllersAdmin = new Object[6];
+        ModelMenuAdmin modelMenuAdmin = new ModelMenuAdmin();
+        ViewMenuAdmin viewMenuAdmin = new ViewMenuAdmin();
+        ControllerMenuAdmin controllerMenuAdmin = new ControllerMenuAdmin(modelMenuAdmin, viewMenuAdmin, controllersAdmin);
+       
+        Object[] controllersLogin = new Object[6];
+        controllersLogin[0] = controllerMenuAdmin;
+        
+        ModelLogin modelLogin = new ModelLogin();
+        ViewLogin viewLogin = new ViewLogin();
+        ControllerLogin controllerLogin = new ControllerLogin(modelLogin,viewLogin,controllersLogin);
+        
+      
+        
     }
     
 }
