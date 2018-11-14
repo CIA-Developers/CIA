2570b7fd3f8d38b3a0e2d68e9f84a1345e1deadd
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-13 21:38:36 -0600

    Se realiza el Run Main de EmpleadosVentas, EmpleadosVentas

--- src/main/MenuEmpleadoCompras.java
+++ src/main/MenuEmpleadoCompras.java
@@ -4,10 +4,9 @@
  * and open the template in the editor.
  */
 package main;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import models.*;
 import views.*;
+import models.*;
+import controllers.*;
 /**
  *
  * @author Octaviano
@@ -18,7 +17,37 @@ public class MenuEmpleadoCompras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ModelClientesEmpleados modelClientesEmpleados = new ModelClientesEmpleados();
+        ViewClientesEmpleados viewClientesEmpleados = new ViewClientesEmpleados();
+        ControllerClientesEmpleados controllerClientesEmpleados = new ControllerClientesEmpleados(modelClientesEmpleados, viewClientesEmpleados);
+        
+        ModelProductosEmpleados modelProductosEmpleados = new ModelProductosEmpleados();
+        ViewProductosEmpleados viewProductosEmpleados = new ViewProductosEmpleados();
+        ControllerProductosEmpleados controllerProductosEmpleados = new ControllerProductosEmpleados(modelProductosEmpleados, viewProductosEmpleados);
+        
+        ModelProveedoresEmpleados modelProveedoresEmpleados = new ModelProveedoresEmpleados();
+        ViewProveedoresEmpleados viewProveedoresEmpleados = new ViewProveedoresEmpleados();
+        ControllerProveedoresEmpleados controllerProveedoresEmpleados = new ControllerProveedoresEmpleados(modelProveedoresEmpleados, viewProveedoresEmpleados);
+        
+        ModelSucursalesEmpleados modelSucursalesEmpleados  = new ModelSucursalesEmpleados ();
+        ViewSucursalesEmpleados  viewSucursalesEmpleados = new ViewSucursalesEmpleados ();
+        ControllerSucursalesEmpleados controllerSucursalesEmpleados  = new ControllerSucursalesEmpleados (modelSucursalesEmpleados , viewSucursalesEmpleados );
+        
+        ModelCOMPRAS modelCOMPRAS = new ModelCOMPRAS();
+        ViewCOMPRAS viewCOMPRAS = new ViewCOMPRAS();
+        ControllerCOMPRAS controllerCOMPRAS = new ControllerCOMPRAS(modelCOMPRAS,viewCOMPRAS);
+        
+        Object[] controllers = new Object[6];
+        controllers[0] = controllerClientesEmpleados;
+        controllers[1] = controllerProductosEmpleados;
+        controllers[2] = controllerProveedoresEmpleados;
+        controllers[3] = controllerSucursalesEmpleados;
+        controllers[4] = controllerCOMPRAS;
+        
+        ModelMenuEmpleadoCompras modelMenuEmpleadoCompras = new ModelMenuEmpleadoCompras ();
+        viewMenuEmpleadoCompras viewMenuEmpleadoCompras = new viewMenuEmpleadoCompras();
+        ControllerMenuEmpleadoCompras controllerMenuEmpleadoCompras = new ControllerMenuEmpleadoCompras(modelMenuEmpleadoCompras,viewMenuEmpleadoCompras,controllers);
+        
     }
     
 }
--- src/main/MenuEmpleadoVentas.java
+++ src/main/MenuEmpleadoVentas.java
@@ -4,10 +4,9 @@
  * and open the template in the editor.
  */
 package main;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import models.*;
 import views.*;
+import models.*;
+import controllers.*;
 /**
  *
  * @author Octaviano
@@ -18,7 +17,37 @@ public class MenuEmpleadoVentas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ModelClientesEmpleados modelClientesEmpleados = new ModelClientesEmpleados();
+        ViewClientesEmpleados viewClientesEmpleados = new ViewClientesEmpleados();
+        ControllerClientesEmpleados controllerClientesEmpleados = new ControllerClientesEmpleados(modelClientesEmpleados, viewClientesEmpleados);
+        
+        ModelProductosEmpleados modelProductosEmpleados = new ModelProductosEmpleados();
+        ViewProductosEmpleados viewProductosEmpleados = new ViewProductosEmpleados();
+        ControllerProductosEmpleados controllerProductosEmpleados = new ControllerProductosEmpleados(modelProductosEmpleados, viewProductosEmpleados);
+        
+        ModelProveedoresEmpleados modelProveedoresEmpleados = new ModelProveedoresEmpleados();
+        ViewProveedoresEmpleados viewProveedoresEmpleados = new ViewProveedoresEmpleados();
+        ControllerProveedoresEmpleados controllerProveedoresEmpleados = new ControllerProveedoresEmpleados(modelProveedoresEmpleados, viewProveedoresEmpleados);
+        
+        ModelSucursalesEmpleados modelSucursalesEmpleados  = new ModelSucursalesEmpleados ();
+        ViewSucursalesEmpleados  viewSucursalesEmpleados = new ViewSucursalesEmpleados ();
+        ControllerSucursalesEmpleados controllerSucursalesEmpleados  = new ControllerSucursalesEmpleados (modelSucursalesEmpleados , viewSucursalesEmpleados );
+        
+        modelVENTAS modelVENTAS  = new modelVENTAS ();
+        ViewVENTAS  viewVENTAS  = new ViewVENTAS ();
+        ControllerVENTAS  controllerVENTAS  = new ControllerVENTAS (modelVENTAS ,viewVENTAS );
+        
+        Object[] controllers = new Object[6];
+        controllers[0] = controllerClientesEmpleados;
+        controllers[1] = controllerProductosEmpleados;
+        controllers[2] = controllerProveedoresEmpleados;
+        controllers[3] = controllerSucursalesEmpleados;
+        controllers[4] = controllerVENTAS ;
+        
+        ModelMenuEmpleadoVentas modelMenuEmpleadoVentas = new ModelMenuEmpleadoVentas ();
+        viewMenuEmpleadoVentas viewMenuEmpleadoVentas = new viewMenuEmpleadoVentas();
+        ControllerMenuEmpleadoVentas controllerMenuEmpleadoVentas = new  ControllerMenuEmpleadoVentas(modelMenuEmpleadoVentas,viewMenuEmpleadoVentas,controllers);
+        
     }
     
 }
