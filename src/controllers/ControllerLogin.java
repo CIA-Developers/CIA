80da6962d6635118983002a03df96cc89f1ead2b
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-13 18:34:22 -0600

    controller login (incompleto)

--- src/controllers/ControllerLogin.java
+++ src/controllers/ControllerLogin.java
@@ -6,8 +6,9 @@
 package controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import models.ModelLogin; // todos los modelos
-import views.ViewLogin; //todas las vistas 
+import models.*; // todos los modelos
+import views.*; //todas las vistas 
+import main.*; //importamos todas la clases main
 /**
  *
  * @author Octaviano
@@ -17,6 +18,9 @@ public class ControllerLogin {
    private final ModelLogin modelLogin;
    private final ViewLogin viewLogin;
 
+   MenuAdmin admin;
+   
+   ViewMenuAdmin viewmenuAdmin;
    /**
      * Esta variable almacena las Vistas base 
      * para poder utilizarlos dentro del mismo JFrame.
@@ -36,29 +40,55 @@ public class ControllerLogin {
      * @param viewLogin
      * @param controllers 
      */
-    public ControllerLogin(ModelLogin modelLogin, ViewLogin viewLogin, Object[] controllers) {
+    public ControllerLogin(ModelLogin modelLogin, ViewLogin viewLogin /*, ViewMenuAdmin viewmenuAdmin*//*, viewMenuEmpleadoCompras viewMenuEmpleadoCompras, viewMenuEmpleadoVentas viewMenuEmpleadoVentas*/){
         this.modelLogin = modelLogin;
         this.viewLogin = viewLogin;
-        this.controllers = controllers;
+        viewLogin.jb_aceptar.addActionListener(actionListener); //agrega el evento ActionListener 
+        //this.controllers = controllers;
         initComponets();
-        setControllers();
     }
     
-    /**
-     * Separa cada uno de los controlladores almacendados en controllers, de
-     * esta forma se puede acceder a todas las variables y métodos publicos
-     * de cada uno.
-     */
-    public void setControllers() {
-        controllerMenuAdmin = (ControllerMenuAdmin) controllers[0];
-    }
-    /**
-     * mustra la ventana principal del menuAdmin
-     */
-    private void initComponets(){
+    private void initComponets() {
         viewLogin.setTitle("LOGIN");
         viewLogin.setLocationRelativeTo(null);
         viewLogin.setVisible(true);
     }
-
+    
+        private final ActionListener actionListener = new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+           if (e.getSource() == viewLogin.jb_aceptar) {
+               opciones();
+           }
+        }    
+    };
+    
+    public void opciones(){
+        if (viewLogin.jcb_tipo_admin.getSelectedItem() == "Administrador"){
+            admin.getClass();
+            viewmenuAdmin.setVisible(true);
+//            Object[] controllersAdmin = new Object[6];
+//            ModelMenuAdmin modelMenuAdmin = new ModelMenuAdmin();
+//            ViewMenuAdmin viewMenuAdmin = new ViewMenuAdmin();
+//            ControllerMenuAdmin controllerMenuAdmin = new ControllerMenuAdmin(modelMenuAdmin, viewMenuAdmin, controllersAdmin); 
+//            viewMenuAdmin.setVisible(true);
+//            viewMenuAdmin.revalidate();
+//            viewMenuAdmin.repaint();
+//            viewLogin.setVisible(false); //ocultar el login
+           
+        }
+        else if (viewLogin.jcb_tipo_admin.getSelectedItem() == "Empleado ventas"){         
+//            viewMenuEmpleadoCompras.setLocationRelativeTo(null);
+//            viewMenuEmpleadoCompras.setVisible(true);
+//            viewMenuEmpleadoCompras.setVisible(true); // abrir la vista de empleado compras
+//            viewLogin.setVisible(false); //ocultar el login
+        }
+        else if (viewLogin.jcb_tipo_admin.getSelectedItem() == "Empleado compras"){          
+//            viewMenuEmpleadoCompras.setLocationRelativeTo(null);
+//            viewMenuEmpleadoCompras.setVisible(true);
+//            viewMenuEmpleadoVentas.setVisible(true); // abrir la vista de empleado ventas 
+//            viewLogin.setVisible(false); //ocultar el login
+        }
+    }
+    
 }
