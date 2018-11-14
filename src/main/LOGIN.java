7d356ee7045949a7417b2bf0615d5717c6af067a
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-13 18:35:23 -0600

    java run class login (incompleto)

--- src/main/LOGIN.java
+++ src/main/LOGIN.java
@@ -19,18 +19,10 @@ public class LOGIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Object[] controllersAdmin = new Object[6];
-        ModelMenuAdmin modelMenuAdmin = new ModelMenuAdmin();
-        ViewMenuAdmin viewMenuAdmin = new ViewMenuAdmin();
-        ControllerMenuAdmin controllerMenuAdmin = new ControllerMenuAdmin(modelMenuAdmin, viewMenuAdmin, controllersAdmin);
-       
-        Object[] controllersLogin = new Object[6];
-        controllersLogin[0] = controllerMenuAdmin;
-        
+    
         ModelLogin modelLogin = new ModelLogin();
         ViewLogin viewLogin = new ViewLogin();
-        ControllerLogin controllerLogin = new ControllerLogin(modelLogin,viewLogin,controllersLogin);
+        ControllerLogin controllerLogin = new ControllerLogin(modelLogin,viewLogin/*,viewMenuAdmin*/ /*,controllersLogin*/);
         
       
         
