3d196b4618e03b92ae64b303a3facb2fe9992313
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-15 21:57:35 -0600

    Programacion del metodo para guardar datos de una nueva sucursal a la tabla en ControllerAgregarSucursal

--- src/controllers/ControllerAgregarSucursal.java
+++ src/controllers/ControllerAgregarSucursal.java
@@ -187,6 +187,18 @@ public class ControllerAgregarSucursal {
     }
 
     public void GuardarAgregarSucursal() {
+            // si la variable verificar es igual a 0 se llama al metodo de guardar Nuevo
+        if (modelAgregarSucursal.getVerificar() == 1) {
+            // darle el valor a las variables
+            modelAgregarSucursal.setNo_sucursal(viewAgregarSucursal.jtf_no_sucursal.getText());
+            modelAgregarSucursal.setCalle(viewAgregarSucursal.jtf_calle.getText());
+            modelAgregarSucursal.setColonia(viewAgregarSucursal.jtf_colonia.getText());
+            modelAgregarSucursal.setNumero(viewAgregarSucursal.jtf_numero.getText());
+            modelAgregarSucursal.setTelefono(viewAgregarSucursal.jtf_telefono.getText());
+       
+        
+        
     }
 
+    }
 }
