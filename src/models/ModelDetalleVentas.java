0290da0439b1a2da07b961d70ddfaa7a9cff6fdd
Amairani Diaz <37378984+Cari-Amairani@users.noreply.github.com>
2018-11-18 14:58:57 -0600

    Sea agregan las librerias y la variable que tome el valor en la tabla en el modelo Detalle Ventas

--- src/models/ModelDetalleVentas.java
+++ src/models/ModelDetalleVentas.java
@@ -5,10 +5,61 @@
  */
 package models;
 
+import conectar_tablas.Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
 /**
  *
  * @author Ami
  */
 public class ModelDetalleVentas {
+ //la variable modelo almacenara los tados de la tabla
+        DefaultTableModel modelo_detalle_compra = new DefaultTableModel();
+
+    public DefaultTableModel getModelo_detalle_compra() {
+        return modelo_detalle_compra;
+    }
+
+    public void setModelo_detalle_compra(DefaultTableModel modelo_detalle_compra) {
+        this.modelo_detalle_compra = modelo_detalle_compra;
+    }
+
+     public int rec;//Variable que tomara el valor seleccionado en la tabla 
+
+    public int getRec() {
+        return rec;
+    }
+
+    public void setRec(int rec) {
+        this.rec = rec;
+    }
     
+    public String Limpiar = " ";
+
+    public String getLimpiar() {
+        return Limpiar;
+    }
+
+    public void setLimpiar(String Limpiar) {
+        this.Limpiar = Limpiar;
+    }
+    
+     public int date;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+     
 }
