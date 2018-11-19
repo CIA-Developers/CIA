f005d0ac4b32b5747e207bfc4a2d3f56d8d3770d
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-19 01:43:24 -0600

    calculando iva y subtotal (compras)

--- src/models/ModelCOMPRAS.java
+++ src/models/ModelCOMPRAS.java
@@ -431,6 +431,9 @@ public class ModelCOMPRAS {
       datos[6] = Float.toString(this.getTotal_por_producto());
       model_compras.addRow(datos);
       //*********************calculando el importe, iva y subtotal de la compra***************
+      importe=this.getImporte();
+      iva= importe * 16/100;//calculando el iva
+      subtotal = importe - iva; //calculando el subtotal
     }
     /**
      * metodo para saber la cantidad de filas que tiene la tabla de compras
