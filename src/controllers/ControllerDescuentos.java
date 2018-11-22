9cb0269520175d641e46c8ef1092bee88b742f0b
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-22 09:05:53 -0600

    Importacion de librerias en ControllerDescuentos

--- src/controllers/ControllerDescuentos.java
+++ src/controllers/ControllerDescuentos.java
@@ -7,6 +7,12 @@ package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
 import models.ModelDescuentos;
 import views.ViewDescuentos;
 
