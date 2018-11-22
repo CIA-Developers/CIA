91fbcc058c3d80ad59c3a0147fb03e012c09772d
IvanHernandezOsornio <ivan.herdez@outlook.com>
2018-11-22 09:34:32 -0600

    Metodos completos en ControllerDescuentos

--- src/controllers/ControllerDescuentos.java
+++ src/controllers/ControllerDescuentos.java
@@ -103,6 +103,111 @@ public class ControllerDescuentos {
 
         this.modelDescuentos = modelDescuentos;
         this.viewDescuentos = viewDescuentos;
+            this.viewDescuentos.jtf_buscar.addKeyListener(key); //agregar elevento de keylistener en la caja e texto buscar
+        this.viewDescuentos.jt_vista.addMouseListener(ml);//agregar a la table el evento de MouseListener
+
+        viewDescuentos.jb_guardar.setEnabled(false);
+//El boton guardar aparecera inhabilitado
+        viewDescuentos.jb_eliminar.setEnabled(false);
+//El boton guardar aparecera inhabilitado
+
+        cajas_deshabilitadas();
+        setActionListener();
+        ConexionBD();
+    }
+     /**
+     * Método para agregar el actionListener a cada boton
+     */
+    private void setActionListener() {
+
+        viewDescuentos.jb_nuevo.addActionListener(list);
+        viewDescuentos.jb_modificar.addActionListener(list);
+        viewDescuentos.jb_guardar.addActionListener(list);
+
+    }
+
+    public void ConexionBD() {
+        modelDescuentos.Conectar();
+        modelDescuentos.mostrar();
+        viewDescuentos.jt_vista.setModel(modelDescuentos.getModelo_Descuentos()); //asignar a la tabla los valores correspondientes
+    }
+
+    public void jt_vista_MouseClicked() {
+        viewDescuentos.jb_guardar.setEnabled(false);
+        viewDescuentos.jb_modificar.setEnabled(true);//El boton modificar aparecera habilitado
+        viewDescuentos.jb_nuevo.setEnabled(true);//El boton nuevo aparecera habilitado
+        cajas_deshabilitadas(); // cuando se haga clic en la tabla, las cajas se volveran a deshabilitar 
+        modelDescuentos.setRec(viewDescuentos.jt_vista.getSelectedRow());//a la variable se le asigna el elemento seleccionado en la tabla
+        viewDescuentos.jtf_codigo_descuento.setText(viewDescuentos.jt_vista.getValueAt(modelDescuentos.getRec(), 0).toString());
+        viewDescuentos.jtf_porcentaje.setText(viewDescuentos.jt_vista.getValueAt(modelDescuentos.getRec(), 1).toString());
+        viewDescuentos.jtf_cantidad_puntos.setText(viewDescuentos.jt_vista.getValueAt(modelDescuentos.getRec(), 2).toString());
+  
+    }
+
+    private void cajas_deshabilitadas() {
+        viewDescuentos.jtf_codigo_descuento.setEditable(false);
+        viewDescuentos.jtf_porcentaje.setEditable(false);
+        viewDescuentos.jtf_cantidad_puntos.setEditable(false);
+    }
+
+    private void cajas_habilitadas() {
+        viewDescuentos.jtf_codigo_descuento.setEditable(true);
+        viewDescuentos.jtf_porcentaje.setEditable(true);
+        viewDescuentos.jtf_cantidad_puntos.setEditable(true);
+
+    }
+    //***************** BOTONES Nuevo, Borrar, Guardar y Modificar**************************
+
+    /**
+     * Metodo que limpiara las cajas de texto para ingresar nuevo datos.
+     */
+    public void nuevo_proveedores() {
+        viewDescuentos.jb_guardar.setEnabled(true);//El boton guardar aparecera habilitado
+        viewDescuentos.jb_modificar.setEnabled(false);//El boton modificar aparecera inhabilitado
+        //limpiar cada caja de la Interfaz 
+        modelDescuentos.setVerificar(1);// le da el valor a verificar de cero para identificar un nuevo registroç
+        viewDescuentos.jtf_codigo_descuento.setText(modelDescuentos.getLimpiar());
+        viewDescuentos.jtf_porcentaje.setText(modelDescuentos.getLimpiar());
+        viewDescuentos.jtf_cantidad_puntos.setText(modelDescuentos.getLimpiar());
+        cajas_habilitadas();//llamar al metodo de cajas habilitadas para proceder a escribir un nuevo registro 
+    }
+
+    public void modificar_proveedores() {
+        viewDescuentos.jb_guardar.setEnabled(true);//El boton guardar aparecera habilitado
+        viewDescuentos.jb_modificar.setEnabled(false);//El boton modificar aparecera inhabilitado
+        //limpiar cada caja de la Interfaz 
+        modelDescuentos.setVerificar(2);// le da el valor a verificar de uno para identificar Modifiar registro
+        viewDescuentos.jtf_codigo_descuento.setEditable(false); // el codigo no se puede modificar
+        viewDescuentos.jtf_porcentaje.setEditable(true);
+        viewDescuentos.jtf_cantidad_puntos.setEditable(true);
+       
+    }
+
+    public void Guardar() {
+        // si la variable verificar es igual a 0 se llama al metodo de guardar Nuevo
+        if (modelDescuentos.getVerificar() == 1) {
+            // darle el valor a las variables
+            modelDescuentos.setId(viewDescuentos.jtf_codigo_descuento.getText());
+            modelDescuentos.setNombre(viewDescuentos.jtf_porcentaje.getText());
+            modelDescuentos.setAp_pat(viewDescuentos.jtf_cantidad_puntos.getText());
+            modelDescuentos.Guardar_Nuevo(); // metodo de  insertar nuevo registro           
+        } else {
+            // darle el valor a las variables
+            modelDescuentos.setId(viewDescuentos.jtf_codigo_descuento.getText());
+            modelDescuentos.setNombre(viewDescuentos.jtf_porcentaje.getText());
+            modelDescuentos.setAp_pat(viewDescuentos.jtf_cantidad_puntos.getText());
+            modelDescuentos.Guardar_Modificado();// metodo de  Modificar el registro
+        }
+        //LIMPIAR TABLA 
+        for (int i = 0; i < viewDescuentos.jt_vista.getRowCount(); i++) {
+            modelDescuentos.getModelo_Descuentos().removeRow(i);
+            i -= 1;
+        }
+        //mostrar los nuevos datos 
+        modelDescuentos.mostrar();
+    }
+}
+
 
     }
 
