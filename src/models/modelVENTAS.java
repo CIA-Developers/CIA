23529d60bbae68da07d2d894106b1cf20ee4b249
AngelesOct <1717110255@utectulancingo.edu.mx>
2018-11-29 13:07:55 -0600

    prinipales variables en modelVentas

--- src/models/modelVENTAS.java
+++ src/models/modelVENTAS.java
@@ -23,5 +23,265 @@ import javax.swing.table.TableRowSorter;
  * @author Octaviano
  */
 public class modelVENTAS {
+    //************************* Variables necesarias para compras *****************************
     
+    public String RFC_empleado;
+    public String nombre_empleado;// solo se obtendra este dato, no se almacenara
+    public String apellido_pat_empleado;// solo se obtendra este dato, no se almacenara
+    public String apellido_mat_empleado;// solo se obtendra este dato, no se almacenara
+    public int num_sucursal; //solo se obtendra este dato, no se almacenara. 
+    public String RFC_cliente;
+    public String nombre_cliente;
+    public String apell_pat_cliente; // solo se obtendra este dato, no se almacenara
+    public String apell_mat_cliente; // solo se obtendra este dato, no se almacenara
+    public int puntos_acumulados;
+    public float subtotal;
+    public float iva;
+    public float importe;
+    
+    public int numero_venta;
+    public String codigo_producto;
+    public String nombre_producto;// solo se obtendra este dato, no se almacenara
+    public String tipo_producto;// solo se obtendra este dato, no se almacenara
+    public String marca_producto;// solo se obtendra este dato, no se almacenara
+    public int cantidad_venta = 0;
+    public float precio_venta;
+    public float total_por_producto=0.0f;
+
+    
+    public int codigo_descuento;
+    public int puntos_requeridos;
+    public int porcentaje;
+    public float descuento;
+    
+    public float efectivo;
+    public float cambio;
+    
+    public int puntos_ganados;
+
+    public String getRFC_empleado() {
+        return RFC_empleado;
+    }
+
+    public void setRFC_empleado(String RFC_empleado) {
+        this.RFC_empleado = RFC_empleado;
+    }
+
+    public String getNombre_empleado() {
+        return nombre_empleado;
+    }
+
+    public void setNombre_empleado(String nombre_empleado) {
+        this.nombre_empleado = nombre_empleado;
+    }
+
+    public String getApellido_pat_empleado() {
+        return apellido_pat_empleado;
+    }
+
+    public void setApellido_pat_empleado(String apellido_pat_empleado) {
+        this.apellido_pat_empleado = apellido_pat_empleado;
+    }
+
+    public String getApellido_mat_empleado() {
+        return apellido_mat_empleado;
+    }
+
+    public void setApellido_mat_empleado(String apellido_mat_empleado) {
+        this.apellido_mat_empleado = apellido_mat_empleado;
+    }
+
+    public int getNum_sucursal() {
+        return num_sucursal;
+    }
+
+    public void setNum_sucursal(int num_sucursal) {
+        this.num_sucursal = num_sucursal;
+    }
+
+    public String getRFC_cliente() {
+        return RFC_cliente;
+    }
+
+    public void setRFC_cliente(String RFC_cliente) {
+        this.RFC_cliente = RFC_cliente;
+    }
+
+    public String getNombre_cliente() {
+        return nombre_cliente;
+    }
+
+    public void setNombre_cliente(String nombre_cliente) {
+        this.nombre_cliente = nombre_cliente;
+    }
+
+    public String getApell_pat_cliente() {
+        return apell_pat_cliente;
+    }
+
+    public void setApell_pat_cliente(String apell_pat_cliente) {
+        this.apell_pat_cliente = apell_pat_cliente;
+    }
+
+    public String getApell_mat_cliente() {
+        return apell_mat_cliente;
+    }
+
+    public void setApell_mat_cliente(String apell_mat_cliente) {
+        this.apell_mat_cliente = apell_mat_cliente;
+    }
+
+    public int getPuntos_acumulados() {
+        return puntos_acumulados;
+    }
+
+    public void setPuntos_acumulados(int puntos_acumulados) {
+        this.puntos_acumulados = puntos_acumulados;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public float getIva() {
+        return iva;
+    }
+
+    public void setIva(float iva) {
+        this.iva = iva;
+    }
+
+    public float getImporte() {
+        return importe;
+    }
+
+    public void setImporte(float importe) {
+        this.importe = importe;
+    }
+
+    public int getNumero_venta() {
+        return numero_venta;
+    }
+
+    public void setNumero_venta(int numero_venta) {
+        this.numero_venta = numero_venta;
+    }
+
+    public String getCodigo_producto() {
+        return codigo_producto;
+    }
+
+    public void setCodigo_producto(String codigo_producto) {
+        this.codigo_producto = codigo_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getTipo_producto() {
+        return tipo_producto;
+    }
+
+    public void setTipo_producto(String tipo_producto) {
+        this.tipo_producto = tipo_producto;
+    }
+
+    public String getMarca_producto() {
+        return marca_producto;
+    }
+
+    public void setMarca_producto(String marca_producto) {
+        this.marca_producto = marca_producto;
+    }
+
+    public int getCantidad_venta() {
+        return cantidad_venta;
+    }
+
+    public void setCantidad_venta(int cantidad_venta) {
+        this.cantidad_venta = cantidad_venta;
+    }
+
+    public float getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(float precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public float getTotal_por_producto() {
+        return total_por_producto;
+    }
+
+    public void setTotal_por_producto(float total_por_producto) {
+        this.total_por_producto = total_por_producto;
+    }
+
+    public int getCodigo_descuento() {
+        return codigo_descuento;
+    }
+
+    public void setCodigo_descuento(int codigo_descuento) {
+        this.codigo_descuento = codigo_descuento;
+    }
+
+    public int getPuntos_requeridos() {
+        return puntos_requeridos;
+    }
+
+    public void setPuntos_requeridos(int puntos_requeridos) {
+        this.puntos_requeridos = puntos_requeridos;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public float getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(float descuento) {
+        this.descuento = descuento;
+    }
+
+    public float getEfectivo() {
+        return efectivo;
+    }
+
+    public void setEfectivo(float efectivo) {
+        this.efectivo = efectivo;
+    }
+
+    public float getCambio() {
+        return cambio;
+    }
+
+    public void setCambio(float cambio) {
+        this.cambio = cambio;
+    }
+
+    public int getPuntos_ganados() {
+        return puntos_ganados;
+    }
+
+    public void setPuntos_ganados(int puntos_ganados) {
+        this.puntos_ganados = puntos_ganados;
+    }
+    
+      
 }
